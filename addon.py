2ef976e00943363bf2b32e986510ee92a24a02f4
darosior <darosior@gmail.com>
2018-08-26 04:47:17 +0200

    Added a new ubenu and new images

--- addon.py
+++ addon.py
@@ -5,27 +5,35 @@ import xbmc
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
+import json
 
 
 class Plugin:
-    def __init__(self, base_url, proc_id, query=''):
-        self.base_url = str(base_url)
+    def __init__(self, base_url, proc_id, query):
+        self.base_url = base_url
         self.proc_id = int(proc_id)
         self.query = query
         self.addon = xbmcaddon.Addon(id='plugin.video.darostream')
+        # To be suitible with all OSs
         self.dir = xbmc.translatePath(self.addon.getAddonInfo('path'))
         self.images_dir = os.path.join(self.dir, 'resources', 'media')
 
     def run(self):
-        self.display_menu()
+        if self.query == '':
+            self.display_main_menu()
+        else:
+            q = self.parse_query()
+            if q['setDirectory'] == 'movies':
+                self.display_movies_menu()
+            elif q['setDirectory'] == 'football':
+                pass
 
-    def display_menu(self):
-        # Main menu
+    def display_main_menu(self):
         # Items to be displayed
         films = xbmcgui.ListItem('Films')
         films.setArt({
             'fanart': os.path.join(self.images_dir, 'fanart_films.jpg'),
-            'icon': os.path.join(self.images_dir, 'icon_films.png')
+            'icon': os.path.join(self.images_dir, 'icon_movies.png')
         })
         foot = xbmcgui.ListItem('Football')
         foot.setArt({
@@ -34,13 +42,38 @@ class Plugin:
         })
         # (url, ListItem, IsDirectory?)
         items = [
-            ('', films, True),
-            ('', foot, True),
+            (self.base_url+'?setDirectory=movies', films, True),
+            (self.base_url+'?setDirectory=movies', foot, True),
         ]
         xbmcplugin.setContent(self.proc_id, 'movies')
         # Last param for progressbar
         xbmcplugin.addDirectoryItems(self.proc_id, items, len(items))
         xbmcplugin.endOfDirectory(self.proc_id)
+
+    def display_movies_menu(self):
+        # Items to be displayed
+        search = xbmcgui.ListItem('Rechercher')
+        search.setArt({
+            'fanart': os.path.join(self.images_dir, 'fanart_films.jpg'),
+            'icon': os.path.join(self.images_dir, 'icon_search.png')
+        })
+        # (url, ListItem, IsDirectory?)
+        items = [
+            ('', search, True),
+        ]
+        xbmcplugin.setContent(self.proc_id, 'movies')
+        # Last param for progressbar
+        xbmcplugin.addDirectoryItems(self.proc_id, items, len(items))
+        xbmcplugin.endOfDirectory(self.proc_id)
+
+    def parse_query(self):
+        queries = {}
+        # Parsing query of form ?a=1&b=2 in [a=1, b=2]
+        tmp = self.query.replace('?', '').encode().split('&')
+        for i in tmp:
+            q = i.split('=')
+            queries[q[0]] = q[1]
+        return queries
 
 if __name__ == '__main__':
     plugin = Plugin(sys.argv[0], sys.argv[1], sys.argv[2])
